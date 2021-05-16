3c51df261f9c0ac3b3d8486b43b85a39b72a553e
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-05-16 15:13:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,11 @@ function findEmployeeByFirstName(srcArray, firstName) {
   return srcArray.find(e => e.firstName === firstName)
 }
 
-
+function calculatePayroll(arrOfEmployee) {
+  return arrOfEmployee.reduce((acc, cur) => {
+    return acc + wagesEarnedOnDate.call(this, cur)
+  }, 0)
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
