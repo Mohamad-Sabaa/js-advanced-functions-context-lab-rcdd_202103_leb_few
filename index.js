47084d0e2ba42ccbd6c4f173f88edc9019c00832
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-05-16 14:27:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
 /* Your Code Here */
+function createEmployeeRecord(emp) {
+  return {
+    firstName: emp[0],
+    familyName: emp[1],
+    title: emp[2],
+    payPerHour: emp[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+function createEmployeeRecords(arr) {
+  return arr.map(emp => createEmployeeRecord(emp))
+  return createEmployeeRecord(emp)
+}
+
+function createTimeInEvent (dateStamp) {
+  let [date, hour] = dateStamp.split(' ')
+  this.timeInEvents.push({
+    type: "TimeIn",
+    date,
+    hour: parseInt(hour)
+  })
+  return this;
+}
+
+ function createTimeOutEvent(dateStamp) {
+   let [date, hour] = dateStamp.split(' ')
+   this.timeOutEvents.push({
+     type: "TimeOut",
+     date,
+     hour: parseInt(hour)
+   })
+   return this;
+ }
+
+ function hoursWorkedOnDate(date) {
+   let timeIn = this.timeInEvents.find(e => e.date === date)
+   let timeOut = this.timeOutEvents.find(e => e.date === date)
+   return (timeOut.hour - timeIn.hour) / 100;
+ }
+
+ function wagesEarnedOnDate(date){
+   let pay = hoursWorkedOnDate(this, date) * this.payPerHour;
+   return parseInt(pay);
+ }
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
